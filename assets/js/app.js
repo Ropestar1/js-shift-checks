a79a52745dfb83e2e5e4a515f3b0835c88ddd665
HQ <5462117+Ropestar1@users.noreply.github.com>
2017-09-24 11:28:59 -0700

    Finished.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,20 @@
-// ## Array Cardio Day 2
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+console.log(checkboxes);
 
-const people = [
-  { name: 'Wes', year: 1988 },
-  { name: 'Kait', year: 1986 },
-  { name: 'Irv', year: 1970 },
-  { name: 'Lux', year: 2015 }
-];
+let lastChecked;
 
-const comments = [
-  { text: 'Love this!', id: 523423 },
-  { text: 'Super good', id: 823423 },
-  { text: 'You are the best', id: 2039842 },
-  { text: 'Ramen is my fav food ever', id: 123523 },
-  { text: 'Nice Nice Nice!', id: 542328 }
-];
+function handleCheck(e) {
+  // console.log(e);
+  let inBetween = false; // NOTE: flag variable: variable indicating the start of a process
+  // e.shiftKey is true when the shiftKey is being held down & checks if checkbox is being checked.
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) inBetween = !inBetween; // toggles Boolean
+      if (inBetween) checkbox.checked = true;
+    })
+  }
 
-// Some and Every Checks
-// Array.prototype.some() // is at least one person 19 or older?
-// const isAdult = people.some(function(person) {
-//   const currentYear = (new Date()).getFullYear();
-//   if (currentYear - person.year >= 19) {
-//     return true
-//   }
-// });
+  lastChecked = this;
+}
 
-// NOTE: Below line of code is equivalent to above lines 19-25
-const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
-console.log(isAdult);
-
-// Array.prototype.every() // is everyone 19 or older?
-const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
-console.log(allAdults);
-
-// Array.prototype.find()
-// Find is like filter, but instead returns just the one you are looking for
-// find the comment with the ID of 823423
-// NOTE: below line includes an implicit return of true
-const comment = comments.find(comment => comment.id === 823423)
-console.log(comment);
-
-
-// Array.prototype.findIndex()
-// Find the comment with this ID
-// delete the comment with the ID of 823423
-const index = comments.findIndex(comment => comment.id === 823423);
-// console.log(index);
-// comments.slice(index, 1);
-const newComments = [
-  ...comments.slice(0, index),
-  ...comments.slice(index + 1)
-];
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
